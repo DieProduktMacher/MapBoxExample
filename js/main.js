4c85f666e3e1ea50db6dfdf36e1f63ba3786c538
sla <stefan.langer@produktmacher.com>
2014-01-23 14:52:27 +0100

    some more mapBox features included

--- js/main.js
+++ js/main.js
@@ -36,22 +36,77 @@ $(document).ready(function(){
       map.panTo(e.layer.getLatLng());
     });
 
-  // Programatically open Popup
+  // Programatically open Popup on Click
   $('.show-city').click(function( event ){
     event.preventDefault();
-    clickButton($(this).data('show-marker'));
-  });
-
-  function clickButton(markerTitle) {
+    var markerTitle = $(this).data('show-marker');
     markerLayer.eachLayer(function(marker) {
-        // you can replace this test for anything else, to choose the right
-        // marker on which to open a popup. by default, popups are exclusive
-        // so opening a new one will close all of the others.
         if (marker.feature.properties.title === markerTitle) {
           marker.openPopup();
         }
       });
+  });
+
+
+
+  /**************** MARKER FILTERS ****************/
+
+
+// find and store a variable reference to the list of filters
+var filters = document.getElementById('filters');
+
+// Wait until the marker layer is loaded in order to build a list of possible
+// types. If you are doing this with another markerLayer, you should change
+// map.markerLayer to the variable you have assigned to your markerLayer
+map.markerLayer.on('ready', function() {
+  // collect the types of symbols in this layer. you can also just
+  // hardcode an array of types if you know what you want to filter on,
+  // like
+  var types = ['city', 'restaurant'];
+
+  var checkboxes = [];
+  // create a filter interface
+  for (var i = 0; i < types.length; i++) {
+
+    // create an <li> list element for each type, and add an input checkbox
+    // and label inside
+    var li = filters.appendChild(document.createElement('li'));
+    var checkbox = li.appendChild(document.createElement('input'));
+    var label = li.appendChild(document.createElement('label'));
+    checkbox.type = 'checkbox';
+    checkbox.id = types[i];
+    checkbox.checked = true;
+    // create a label to the right of the checkbox with explanatory text
+    label.innerHTML = types[i];
+    label.setAttribute('for', types[i]);
+    // whenever a person clicks on this checkbox, call the update() function
+    // below
+    checkbox.addEventListener('change', update);
+    checkboxes.push(checkbox);
   }
+
+  // this function is called whenever someone clicks on a checkbox and changes
+  // the selection of markers to be displayed
+  function update() {
+    console.log("update");
+    var enabled = {};
+    // run through each checkbox and record whether it is checked. If it is,
+    // add it to the object of types to display, otherwise do not.
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) enabled[checkboxes[i].id] = true;
+      console.log("checking");
+    }
+    markerLayer.setFilter(function(feature) {
+      console.log("Set Filter");
+      // if this symbol is in the list, return true. if not, return false.
+      // the 'in' operator in javascript does exactly that: given a string
+      // or number, it says if that is in a object
+      // 2 in { 2: true } // true
+      // 2 in { } // false
+      return (feature.properties['category'] in enabled);
+    });
+  }
+});
 }
 });
 
