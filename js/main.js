26b8b65b1c742ae304adf4e515acf1f49fbd446e
sla <stefan.langer@produktmacher.com>
2014-01-23 16:36:35 +0100

    added bounds

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 $(document).ready(function () {
 
-    var mapID = 'stepfla.h33ln32g';
-    var markerLayer = null;
+  var mapID = 'stepfla.h33ln32g';
+  var markerLayer = null;
 
     // Create Map
     var map = L.mapbox.map('map', mapID)
-        .setView([48.1204969, 11.5440352], 9)
-        .addControl(L.mapbox.geocoderControl(mapID));
+    .setView([48.1204969, 11.5440352], 9)
+    .addControl(L.mapbox.geocoderControl(mapID));
 
     // Create Marker
     // this feature is in the GeoJSON format: see geojson.org
     // for the full specification
     $.getJSON("js/markers.json", function (data) {
-        markerLayer = L.mapbox.markerLayer(data).addTo(map);
+      markerLayer = L.mapbox.markerLayer(data).addTo(map);
 
         // Since getJSON happens asynchronously everything that depends on the Markers has to be called in here
         prepareMapFunctionalities();
-    });
+      });
 
 
     function prepareMapFunctionalities() {
 
-        /********* Marker Hover ********/
+      /************* Fit Bounds *********/
+
+      $('#fitBounds').click(function(){
+        var southWest = new L.LatLng(40.712, -74.227),
+        northEast = new L.LatLng(40.774, -74.125),
+        bounds = new L.LatLngBounds(southWest, northEast);
+
+        map.fitBounds(bounds);
+      });
+
+
+
+      /********* Marker Hover ********/
 
         // markerLayer.on('mouseover', function (e) {
         //     e.layer.openPopup();
@@ -30,23 +42,25 @@ $(document).ready(function () {
         //     e.layer.closePopup();
         // });
 
-        /********* Custom Popup Window ********/
+/********* Custom Popup Window ********/
         // https://www.mapbox.com/mapbox.js/example/v1.0.0/custom-popup/
 
         // Centering Markers on Click
         markerLayer.on('click', function (e) {
-            map.panTo(e.layer.getLatLng());
+          map.panTo(e.layer.getLatLng());
         });
 
         // Programatically open Popup on Click
         $('.show-city').click(function (event) {
-            event.preventDefault();
-            var markerTitle = $(this).data('show-marker');
-            markerLayer.eachLayer(function (marker) {
-                if (marker.feature.properties.title === markerTitle) {
-                    marker.openPopup();
-                }
-            });
+          event.preventDefault();
+
+          var markerTitle = $(this).data('show-marker');
+          markerLayer.eachLayer(function (marker) {
+            marker.closePopup();
+            if (marker.feature.properties.title === markerTitle) {
+              marker.openPopup();
+            }
+          });
         });
 
 
@@ -70,29 +84,29 @@ $(document).ready(function () {
             // create checkboxes
             for (var i = 0; i < types.length; i++) {
 
-                var checkboxID = types[i];
-                $filters.append('<li><input type="checkbox" checked id="' + checkboxID + '"><label for="' + checkboxID + '">' + checkboxID + '</li>');
-                $checkbox = $('#' + checkboxID);
-                $checkbox.on('change', update);
-                checkboxes.push($checkbox);
+              var checkboxID = types[i];
+              $filters.append('<li><input type="checkbox" checked id="' + checkboxID + '"><label for="' + checkboxID + '">' + checkboxID + '</li>');
+              $checkbox = $('#' + checkboxID);
+              $checkbox.on('change', update);
+              checkboxes.push($checkbox);
             }
 
             // this function is called whenever someone clicks on a checkbox and changes
             // the selection of markers to be displayed
             function update() {
-                var enabled = {};
+              var enabled = {};
                 // run through each checkbox and record whether it is checked. If it is,
                 // add it to the object of types to display, otherwise do not.
                 for (var i = 0; i < checkboxes.length; i++) {
-                    if (checkboxes[i].is(':checked')) {
-                        enabled[checkboxes[i].attr('id')] = true;
-                    }
+                  if (checkboxes[i].is(':checked')) {
+                    enabled[checkboxes[i].attr('id')] = true;
+                  }
                 }
                 markerLayer.setFilter(function (feature) {
                     // Check if category is enabled
                     return (feature.properties['category'] in enabled);
-                });
-            }
-        });
-    }
+                  });
+              }
+            });
+}
 });
